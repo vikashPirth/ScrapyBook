f53f70009aa5cd4e8d6fe19bfd029700f7793075
vikashPirth <vikash.pirthiani@gmail.com>
2024-10-10 09:59:07 +0200

    BookSpider

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from  books.items import BooksItem
 
 class BookSpider(scrapy.Spider):
     name = "book"
@@ -7,4 +7,9 @@ class BookSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        for book in response.css("article.product_pod"):
+            item = BooksItem()
+            item["url"] = book.css("h3 > a::attr(href)").get()
+            item["title"] = book.css("h3 > a::attr(title)").get()
+            item["price"] = book.css(".price_color::text").get()
+            yield item
