78b65634ab88a6916ba2eb7a050a2fb64aac6939
vikashPirth <vikash.pirthiani@gmail.com>
2024-10-11 20:45:51 +0200

    next page scraping

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -13,3 +13,9 @@ class BookSpider(scrapy.Spider):
             item["title"] = book.css("h3 > a::attr(title)").get()
             item["price"] = book.css(".price_color::text").get()
             yield item
+
+        next_page = response.css("li.next > a::attr(href)").get()
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
+           
