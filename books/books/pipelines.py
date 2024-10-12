a03dc9849aa328083e45ff715ad548a461bdd4fd
vikashPirth <vikash.pirthiani@gmail.com>
2024-10-13 00:19:55 +0200

    avoid duplicate entry in db

--- books/books/pipelines.py
+++ books/books/pipelines.py
@@ -1,6 +1,7 @@
 import hashlib
 import pymongo 
 from itemadapter import ItemAdapter 
+from scrapy.exceptions import DropItem
 
 
 class MongoPipeline:
@@ -29,5 +30,15 @@ class MongoPipeline:
 
     
     def process_item(self, item, spider):
-        self.db[self.COLLECTION_NAME].insert_one(ItemAdapter(item).asdict())
-        return item
+        item_id = self.compute_item_id(item)
+        if self.db[self.COLLECTION_NAME].find_one({"_id":item_id}):
+            raise DropItem(f"Duplicate item found: {item}")
+        else:
+            item["_id"] = item_id
+            self.db[self.COLLECTION_NAME].insert_one(ItemAdapter(item).asdict())
+            return item
+    
+
+    def compute_item_id(self, item):
+        url = item["url"]
+        return hashlib.sha256(url.encode("utf-8")).hexdigest()
